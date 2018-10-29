1087eebf48098ace833a34639e7677df81755efb
James Cassidy <cassidyjamesw@gmail.com>
2018-10-29 14:06:53 -0400

    end points created

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const helmet = require("helmet");
 const server = express();
 
+//import routes
+const notesRoutes = require("./data/helpers/notesRoutes");
+
 server.use(helmet());
 server.use(express.json());
 
@@ -11,5 +14,6 @@ server.get("/", (req, res) => {
 });
 
 //Routes
+server.use("/api/notes", notesRoutes);
 
 server.listen(9000, () => console.log("\nAPI running on port 9000\n"));
